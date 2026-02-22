8ff09fa94ac98df4fda78c3ee13ce9efee1aa8df
Joaquín Fernando Soto Rojas <109157231+jksotorojas@users.noreply.github.com>
2026-02-22 09:34:49 -0600

    Add files via upload

--- assets/js/app/boot.js
+++ assets/js/app/boot.js
@@ -1,7 +1,7 @@
 // Boot SGF v1.17.16
 
 window.SGF = window.SGF || {};
-window.SGF.APP_VERSION = '1.29.1';
+window.SGF.APP_VERSION = '1.29.3';
 
 // Nota: La delegación del Split de movimientos vive en el módulo de Movimientos.
 // Mantenerla aquí causaba doble-toggle (dos listeners capturando el mismo click),
